c757d4e8df158122398a72d81af10088b14d766c
vvakame <vvakame+dev@gmail.com>
2020-11-14 20:48:04 +0900

    add example_test.go

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # gcplogurl ![Build & Test](https://github.com/vvakame/gcplogurl/workflows/Build%20&%20Test/badge.svg)
 
 Build link URL to Google Cloud Platform Cloud Logging [Logs Explorer](https://cloud.google.com/logging/docs/view/logs-viewer-interface).
+
+see [example](./example_test.go).
---
+++ example_test.go
@@ -0,0 +1,43 @@
+package gcplogurl_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/vvakame/gcplogurl"
+	"go.opencensus.io/trace"
+)
+
+func Example() {
+	exp := &gcplogurl.Explorer{
+		ProjectID:    "test-project",
+		StorageScope: gcplogurl.StorageScopeProject,
+		Query:        `trace="projects/test-project/traces/2f029f622a49837d621cbe6dc5e5d146"`,
+		TimeRange: &gcplogurl.SpecificTimeWithRange{
+			At:    time.Date(2020, 11, 14, 4, 20, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)),
+			Range: 2 * time.Hour,
+		},
+	}
+	logURL := exp.String()
+
+	fmt.Println(logURL)
+
+	// Output:
+	// https://console.cloud.google.com/logs/query;query=trace%3D%22projects%2Ftest-project%2Ftraces%2F2f029f622a49837d621cbe6dc5e5d146%22;storageScope=project;timeRange=2020-11-13T19:20:00Z%2F2020-11-13T19:20:00Z--PT2H?project=test-project
+}
+
+func ExampleTraceLogURL() {
+	_ = os.Setenv("GCP_PROJECT", "test-project")
+	ctx := context.Background()
+	ctx, span := trace.StartSpan(ctx, "test-span")
+	defer span.End()
+
+	logURL, err := gcplogurl.TraceLogURL(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(logURL)
+}

ce659d279aebfb05a947ca48d6e59d8f3a095024
vvakame <vvakame+dev@gmail.com>
2020-11-17 21:05:03 +0900

    address lint comment

--- path_encode_test.go
+++ path_encode_test.go
@@ -46,6 +46,7 @@ func Test_values(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			if got := tt.v.Encode(); got != tt.wantEncode {
 				t.Errorf("Encode() = %v, want %v", got, tt.wantEncode)
